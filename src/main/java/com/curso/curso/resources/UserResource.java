762eb8d360cc90889c4a7ed74644788890c11429
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-07-23 10:45:51 -0400

    User Entity and Resource

---
+++ src/main/java/com/curso/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.curso.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Romulo", "romulo@gmail.com", "999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
